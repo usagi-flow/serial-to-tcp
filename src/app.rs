2afb2a548ec74a8eae061aafa5079f1750a83ded
usagi-flow <2804556+usagi-flow@users.noreply.github.com>
2022-04-28 18:49:56 +0200

    Refactoring

--- src/app.rs
+++ src/app.rs
@@ -3,6 +3,7 @@ use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use tokio::io::AsyncReadExt;
 use tokio::io::AsyncWriteExt;
 use tokio::net::TcpListener;
+use tokio::net::TcpStream;
 use tokio::sync::watch;
 use tokio::sync::watch::Receiver;
 use tokio::sync::watch::Sender;
@@ -41,40 +42,39 @@ impl App
 		let (sender, receiver) = watch::channel::<Chunk>(Chunk::default());
 
 		tokio::spawn(async move {
-			self.read_serial(sender).await.unwrap();
+			loop {
+				self.read_serial(&sender).await.unwrap();
+
+				// Wait for the serial port to become available
+				//sleep(std::time::Duration::from_millis(3000)).await;
+			}
 		});
 
 		loop {
 			log::info!("Listening for connections on port {}...", port);
 
-			let (mut socket, peer_address) = listener.accept().await?;
-			let mut receiver_instance = receiver.clone();
-
+			let (socket, peer_address) = listener.accept().await?;
+			let receiver_instance = receiver.clone();
 			tokio::spawn(async move {
-				log::info!("{:?} connected", peer_address);
+				self.serve(socket, peer_address, receiver_instance).await.unwrap();
+			});
+		}
+	}
 
-				loop {
-					if let Ok(()) = receiver_instance.changed().await {
-						// Quickly copy the data into a vec and release the borrowed chunk
-						let data = self.get_data_copy(&receiver_instance).await;
+    async fn serve(&self,
+		mut socket: TcpStream,
+		peer_address: SocketAddr,
+		mut receiver: Receiver<Chunk>) -> Result<(), std::io::Error>
+    {
+		log::info!("{:?} connected", peer_address);
 
-						if data.len() > 0 {
-							if let Err(_) = socket.write_all(data.as_slice()).await {
-								log::warn!("Failed to send data to peer {:?}, disconnecting...",
-									peer_address);
-								socket.shutdown().await?;
+		loop {
+			if let Ok(()) = receiver.changed().await {
+				let data = self.get_data_copy(&receiver).await;
 
-								let result: Result<(), std::io::Error> = Ok(());
-								return result;
-							}
-						}
-						else {
-							log::warn!("Ignoring empty chunk from the serial data sender")
-						}
-					}
-					else {
-						// The sender has been dropped; disconnect gracefully from the peer.
-						log::warn!("Serial data sender has been dropped, disconnecting from peer: {:?}",
+				if data.len() > 0 {
+					if let Err(_) = socket.write_all(data.as_slice()).await {
+						log::warn!("Failed to send data to peer {:?}, disconnecting...",
 							peer_address);
 						socket.shutdown().await?;
 
@@ -82,20 +82,31 @@ impl App
 						return result;
 					}
 				}
-			});
-		}
+				else {
+					log::warn!("Ignoring empty chunk from the serial data sender")
+				}
+			}
+			else {
+				// The sender has been dropped; disconnect gracefully from the peer.
+				log::warn!("Serial data sender has been dropped, disconnecting from peer: {:?}",
+					peer_address);
+				socket.shutdown().await?;
 
-		//return Ok(());
-	}
+				let result: Result<(), std::io::Error> = Ok(());
+				return result;
+			}
+		}
+    }
 
 	async fn get_data_copy(&self, receiver_instance: &Receiver<Chunk>) -> Vec<u8>
 	{
+		// Quickly copy the data into a vec and release the borrowed chunk
 		// TODO: use borrow_and_update() instead?
 		let chunk = receiver_instance.borrow();
 		return chunk.data[0 .. chunk.size].to_vec();
 	}
 
-	pub async fn read_serial(&self, sender: Sender<Chunk>) -> Result<(), std::io::Error>
+	pub async fn read_serial(&self, sender: &Sender<Chunk>) -> Result<(), std::io::Error>
 	{
 		let path = "/dev/ttyUSB0";
 		let baud_rate = 115200u32;
